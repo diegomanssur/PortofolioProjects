923278fa1e79459ed6b6a86c4dd46a3c9542cd10
Diego <159732401+diegomanssur@users.noreply.github.com>
2024-02-23 13:17:26 -0800

    sleep_health_lifestyle.sql

--- sleep_health_lifestyle.sql
+++ sleep_health_lifestyle.sql
@@ -1,8 +1,12 @@
+# Sleep, Health and LifeStyle (Data Exploration)
+
 # Viewing Data
 
 SELECT * 
 FROM sleep_lifestyle_health
 ;
+
+# Occupation Breakdown
 
 # Average Sleep Duration Per Occupation
 
@@ -31,79 +35,103 @@ GROUP BY Occupation
 ORDER BY Average_Daily_Steps DESC
 ;
 
-# Correlation between Age and Sleep Duration
+# Average Stress Level Per Occupation
 
-SELECT Age, 
-ROUND(AVG(`Sleep Duration`),2) AS Average_Sleep_Duration
-FROM sleep_lifestyle_health
-GROUP BY Age
-ORDER BY Age DESC
-;
-
-# Correlation between Sleep Duration and Sleep Quality
-
-SELECT `Sleep Duration`, 
-ROUND(AVG(`Quality of Sleep`),2) AS Average_Quality_of_Sleep
-FROM sleep_lifestyle_health
-GROUP BY `Sleep Duration`
-ORDER BY `Sleep Duration` DESC
-;
-
-# Correlation between Age and Sleep Quality
-
-SELECT Age, 
-ROUND(AVG(`Quality of Sleep`),2) AS Average_Quality_of_Sleep
-FROM sleep_lifestyle_health
-GROUP BY Age
-ORDER BY Age DESC
-;
-
-# Correlation between Quality of Sleep vs Average Number of Steps
-
-SELECT `Quality of Sleep`,
-ROUND(AVG(`Daily Steps`),2) AS Average_Daily_Steps
-FROM sleep_lifestyle_health
-GROUP BY `Quality of Sleep`
-ORDER BY `Quality of Sleep` DESC
-;
-
-# Correlation between Quality of Sleep vs Stress Level
-
-SELECT `Quality of Sleep`,
+SELECT Occupation, 
 ROUND(AVG(`Stress Level`),2) AS Average_Stress_Level
 FROM sleep_lifestyle_health
-GROUP BY `Quality of Sleep`
-ORDER BY `Quality of Sleep` DESC
+GROUP BY Occupation
+ORDER BY Average_Stress_Level DESC
 ;
 
-# Correlation betweeen Sleep Duration and Physical Activity Level
 
-SELECT `Sleep Duration`,
-ROUND(AVG(`Physical Activity Level`),2) AS Average_Physical_Activity_Level
+# Correlations
+
+# Correlation between Sleep Duration and Age per Occupation
+
+SELECT Occupation, 
+ROUND(AVG(`Sleep Duration`),2) AS Average_Sleep_Duration,
+ROUND(AVG(`Age`),2) AS Average_Age
 FROM sleep_lifestyle_health
-GROUP BY `Sleep Duration`
-ORDER BY `Sleep Duration` DESC
+GROUP BY Occupation
+ORDER BY Average_Sleep_Duration DESC
 ;
 
-# Correlation betweeen Quality of Sleep and Physical Activity Level
+# Correlation between Sleep Quality and Age per Occupation
 
-SELECT `Quality of Sleep`,
-ROUND(AVG(`Physical Activity Level`),2) AS Average_Physical_Activity_Level
+SELECT Occupation, 
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
+ROUND(AVG(`Age`),2) AS Average_Age
 FROM sleep_lifestyle_health
-GROUP BY `Quality of Sleep`
-ORDER BY `Quality of Sleep` DESC
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
 ;
 
-# Correlation betweeen Daily Steps and Heart Rate
+# Correlation between Sleep Duration and Sleep Quality per Occupation
 
-SELECT `Heart Rate`,
+SELECT Occupation, 
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
+ROUND(AVG(`Sleep Duration`),2) AS Average_Sleep_Duration
+FROM sleep_lifestyle_health
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
+;
+
+# Correlation between Sleep Quality and Daily Steps per Occupation
+
+SELECT Occupation,
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
 ROUND(AVG(`Daily Steps`),2) AS Average_Daily_Steps
 FROM sleep_lifestyle_health
-GROUP BY `Heart Rate`
-ORDER BY `Heart Rate` DESC
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
 ;
 
-# Correlation between BMI Category and Insomnia
+# Correlation between Sleep Quality and Stress Level per Occupation
+
+SELECT Occupation,
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
+ROUND(AVG(`Stress Level`),2) AS Average_Stress_Level
+FROM sleep_lifestyle_health
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
+;
+
+# Correlation betweeen Sleep Duration and Physical Activity Level per Occupation
+
+SELECT Occupation,
+ROUND(AVG(`Sleep Duration`),2) AS Average_Sleep_Duration,
+ROUND(AVG(`Physical Activity Level`),2) AS Average_Physical_Activity_Level
+FROM sleep_lifestyle_health
+GROUP BY Occupation
+ORDER BY Average_Sleep_Duration DESC
+;
+
+# Correlation betweeen Sleep Quality and Physical Activity Level per Occupation
+
+SELECT Occupation,
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
+ROUND(AVG(`Physical Activity Level`),2) AS Average_Physical_Activity_Level
+FROM sleep_lifestyle_health
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
+;
+
+# Correlation betweeen Sleep Quality and Daily Steps
+
+SELECT Occupation,
+ROUND(AVG(`Quality of Sleep`),2) AS Average_Sleep_Quality,
+ROUND(AVG(`Daily Steps`),2) AS Average_Daily_Steps
+FROM sleep_lifestyle_health
+GROUP BY Occupation
+ORDER BY Average_Sleep_Quality DESC
+;
+
+
+
+# Sleep Disorders per BMI Category
+
+# BMI Category and Insomnia
 
 SELECT `BMI Category`, COUNT(`Sleep Disorder`) AS Insomnia
 FROM sleep_lifestyle_health
@@ -111,7 +139,7 @@ WHERE `Sleep Disorder` = 'Insomnia'
 GROUP BY `BMI Category`
 ;
 
-# Correlation between BMI Category and Sleep Apnea
+# BMI Category and Sleep Apnea
 
 SELECT `BMI Category`, COUNT(`Sleep Disorder`) AS Sleep_Apnea
 FROM sleep_lifestyle_health
@@ -119,7 +147,7 @@ WHERE `Sleep Disorder` = 'Sleep Apnea'
 GROUP BY `BMI Category`
 ;
 
-# Correlation between BMI Category and No Sleep Disorder
+# BMI Category and No Sleep Disorder
 
 SELECT `BMI Category`, COUNT(`Sleep Disorder`) AS None
 FROM sleep_lifestyle_health
@@ -128,4 +156,19 @@ GROUP BY `BMI Category`
 ;
 
 
+# Quantity and Quality of Sleep per BMI Category
+
+# Average Quality of Sleep per BMI Category
+
+SELECT `BMI Category`, ROUND(AVG(`Quality of Sleep`),2) AS Avg_Quality_of_Sleep
+FROM sleep_lifestyle_health
+GROUP BY `BMI Category`
+;
+
+# Average Quality of Sleep per BMI Category
+
+SELECT `BMI Category`, ROUND(AVG(`Sleep Duration`),2) AS Avg_Sleep_Duration
+FROM sleep_lifestyle_health
+GROUP BY `BMI Category`
+;
 
