fe095b8320fe320d1124dbee3bbc9ff1fbbb561c
Diego <159732401+diegomanssur@users.noreply.github.com>
2024-03-03 21:11:58 -0800

    world_life_expectancy_data_exploration.sql

--- world_life_expectancy_data_exploration.sql
+++ world_life_expectancy_data_exploration.sql
@@ -28,6 +28,15 @@ GROUP BY Year
 ORDER BY Year
 ;
 
+
+# Average Life Expectancy between 2007 and 2022
+
+SELECT ROUND(AVG(Lifeexpectancy),2) AS Avg
+FROM world_life_expectancy_001
+WHERE Lifeexpectancy <> 0
+;
+
+
 # Correlation Between Life Expectancy and GDP
 
 SELECT Country, ROUND(AVG(Lifeexpectancy),2) AS Life_Exp, ROUND(AVG(GDP),2) AS GDP
@@ -41,20 +50,20 @@ ORDER BY GDP DESC
 
 SELECT 
 SUM(CASE WHEN GDP >= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
-AVG(CASE WHEN GDP >= 1500 THEN Lifeexpectancy ELSE NULL END) High_GDP_Count_Life_Expectancy,
+ROUND(AVG(CASE WHEN GDP >= 1500 THEN Lifeexpectancy ELSE NULL END),2) High_GDP_Count_Life_Expectancy,
 SUM(CASE WHEN GDP <= 1500 THEN 1 ELSE 0 END) Low_GDP_Count,
-AVG(CASE WHEN GDP <= 1500 THEN Lifeexpectancy ELSE NULL END) Low_GDP_Count_Life_Expectancy
+ROUND(AVG(CASE WHEN GDP <= 1500 THEN Lifeexpectancy ELSE NULL END),2) Low_GDP_Count_Life_Expectancy
 FROM world_life_expectancy_001
 ;
 
 # Correlation Between Life Expectancy and Status
 
-SELECT Status, ROUND(AVG(Lifeexpectancy),1)
+SELECT Status, ROUND(AVG(Lifeexpectancy),1) AS Average_Life_Expectancy
 FROM world_life_expectancy_001
 GROUP BY Status
 ;
 
-SELECT Status, COUNT(DISTINCT Country), ROUND(AVG(Lifeexpectancy),1)
+SELECT Status, COUNT(DISTINCT Country) AS Number_of_Countries, ROUND(AVG(Lifeexpectancy),1) AS Average_Life_Expectancy
 FROM world_life_expectancy_001
 GROUP BY Status
 ;
@@ -69,7 +78,20 @@ AND BMI <> 0
 ORDER BY BMI ASC
 ;
 
-# Adult Mortality
+# Correlation between Life Expectancy and Adult Mortality
+
+SELECT Country,
+ROUND(AVG(Lifeexpectancy),2) AS Avg_Life_Expectancy,
+ROUND(AVG(AdultMortality),2) AS Avg_Adult_Mortality
+#SUM(AdultMortality) AS Total_Adult_Mortality
+FROM world_life_expectancy_001
+GROUP BY Country
+HAVING Avg_Life_Expectancy <>0
+AND Avg_Adult_Mortality<>0
+ORDER BY Avg_Adult_Mortality DESC
+;
+
+# Adult Mortality Rolling Total
 
 SELECT Country,
 Year,
@@ -78,5 +100,3 @@ AdultMortality,
 SUM(AdultMortality)OVER (PARTITION BY Country ORDER BY Year) AS Rolling_Total
 FROM world_life_expectancy_001
 ;
-
-
